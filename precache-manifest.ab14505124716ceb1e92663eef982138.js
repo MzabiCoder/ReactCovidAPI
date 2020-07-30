4c205907048b5525bc0720b0516c490c733904d6
Nabil Fannane <nabil@goadroit.com>
2020-07-29 17:30:02 -0700

    Updates

---
+++ precache-manifest.ab14505124716ceb1e92663eef982138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44698a1a83c839a5275c096d3dd3fdca",
+    "url": "/ReactCovidAPI/index.html"
+  },
+  {
+    "revision": "3614d0d8431a60c049ec",
+    "url": "/ReactCovidAPI/static/css/main.9fefcbd1.chunk.css"
+  },
+  {
+    "revision": "80e83ce289ea85940cae",
+    "url": "/ReactCovidAPI/static/js/2.e3f179de.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/ReactCovidAPI/static/js/2.e3f179de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3614d0d8431a60c049ec",
+    "url": "/ReactCovidAPI/static/js/main.a04ad66f.chunk.js"
+  },
+  {
+    "revision": "ca6a446cdd6d75fec729",
+    "url": "/ReactCovidAPI/static/js/runtime-main.09c2885e.js"
+  }
+]);
